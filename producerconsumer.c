3fda6dc21ecf06a1e64c4b95043027838ad98bb8
Divyanshu Dobhal <dobhaldivyanshu5@gmail.com>
2026-09-16 20:54:02 +0530

    Update

--- producerconsumer.c
+++ producerconsumer.c
@@ -1,1 +1,90 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define SIZE 10
+
+int buffer[SIZE];
+int in = 0;
+int out = 0;
+
+sem_t empty;
+sem_t full;
+sem_t mutex;
+
+void *producer(void *arg)
+{
+    int item;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        item = i; 
+        sem_wait(&empty);
+
+        // >critical section
+        sem_wait(&mutex);
+
+        buffer[in] = item;
+        printf("Producer produces item %d\n", item);
+
+        in = (in + 1) % SIZE;
+
+        // < critical section
+        sem_post(&mutex);
+
+        sem_post(&full);
+        sleep(1);
+    }
+    return NULL;
+}
+
+
+void *consumer(void *arg)
+{
+    int item;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        sem_wait(&full);
+
+        //>critical section
+        sem_wait(&mutex);
+
+        item = buffer[out];
+        printf("Consumer consumes item %d\n", item);
+
+        out = (out + 1) % SIZE;
+
+        // < critical section
+        sem_post(&mutex);
+
+        sem_post(&empty);
+        sleep(1);
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t producerThread, consumerThread;
+
+    sem_init(&empty, 0, SIZE);
+    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+
+    pthread_create(&producerThread, NULL, producer, NULL);
+    pthread_create(&consumerThread, NULL, consumer, NULL);
+
+    pthread_join(producerThread, NULL);
+    pthread_join(consumerThread, NULL);
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    sem_destroy(&mutex);
+
+    return 0;
+}
