081ea2fd80cc1062b4b36a41a5974c3a73d5d853
Divyanshu Dobhal <dobhaldivyanshu5@gmail.com>
2026-09-22 20:46:07 +0530

    Update producerconsumer.c

--- producerconsumer.c
+++ producerconsumer.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -15,16 +14,19 @@ sem_t empty;
 sem_t full;
 sem_t mutex;
 
+// Producer
 void *producer(void *arg)
 {
     int item;
 
     for (int i = 1; i <= 10; i++)
     {
-        item = i; 
+        item = i;
+
+        // Wait for an empty slot
         sem_wait(&empty);
 
-        // >critical section
+        // Enter critical section
         sem_wait(&mutex);
 
         buffer[in] = item;
@@ -32,25 +34,29 @@ void *producer(void *arg)
 
         in = (in + 1) % SIZE;
 
-        // < critical section
+        // Leave critical section
         sem_post(&mutex);
 
+        // Increase full slots
         sem_post(&full);
+
         sleep(1);
     }
+
     return NULL;
 }
 
-
+// Consumer
 void *consumer(void *arg)
 {
     int item;
 
     for (int i = 1; i <= 10; i++)
     {
+        // Wait for a full slot
         sem_wait(&full);
 
-        //>critical section
+        // Enter critical section
         sem_wait(&mutex);
 
         item = buffer[out];
@@ -58,10 +64,12 @@ void *consumer(void *arg)
 
         out = (out + 1) % SIZE;
 
-        // < critical section
+        // Leave critical section
         sem_post(&mutex);
 
+        // Increase empty slots
         sem_post(&empty);
+
         sleep(1);
     }
 
@@ -72,6 +80,7 @@ int main()
 {
     pthread_t producerThread, consumerThread;
 
+    
     sem_init(&empty, 0, SIZE);
     sem_init(&full, 0, 0);
     sem_init(&mutex, 0, 1);
